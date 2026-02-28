f65de1f5bc3ad675545e4ab149d9b7a04f879816
Manvesh-Dudhatra <dudhatramanvesh23@gmail.com>
2026-02-28 12:51:42 +0530

    initial-backend-setup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SentinelAI
+
+📥 Installation Guide
+
+1️⃣ Clone the Repository
+    git clone https://github.com/Manvesh-Dudhatra/SentinelAI.git
+
+2️⃣ Install Python 3.11
+
+3️⃣ Install Pipenv
+    pip install --user pipenv
+
+4️⃣ Navigate to Project Root
+    cd SentinelAI
+
+5️⃣ Install Dependencies
+    pipenv install
+
+🗄 Database Setup
+    Run Migrations
+        pipenv run python manage.py makemigrations
+        pipenv run python manage.py migrate
+
+▶️ Run Development Server
+    pipenv run python manage.py runserver
---
+++ backend/api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, editable=False, default = uuid.uuid4())
+    is_verified = models.BooleanField(default=False)
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default = uuid.uuid4())
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    emp_code = models.CharField(max_length=6)
+    name = models.CharField(max_length= 30, null=True)
+    mobile = models.CharField(max_length=10, null=True)
+    dob = models.DateField(null=True)
+    designation = models.CharField(max_length= 20, null=True)
+    department = models.CharField(max_length= 10, null=True)
+    date_joined = models.DateField(null=True)
+    is_active = models.BooleanField(default=False, null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
+    update_at = models.DateField(auto_now=True, null=True)
+
+class Attendence(models.Model):
+    STATUS = (
+        ("ACTIVE", "Active"),
+        ("PAUSED", "Paused"),
+        ("ENDED", "Ended"),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="sessions")
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+    total_work_time = models.DurationField(null=True, blank=True)
+    total_idle_time = models.DurationField(null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS, default="ACTIVE")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status
+
+class FaceData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="faces")
+    embedding = models.BinaryField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
